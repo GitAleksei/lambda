34d62ce6adde46aaf96f4aa741f3c55ca70fd2a4
Kiva Aleksey <adolfp@mail.ru>
2021-05-07 18:11:46 +0600

    Initing commit

---
+++ task2/Worker.java
@@ -0,0 +1,21 @@
+package lambda.task2;
+
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " not completed. Error.");
+                continue;
+            }
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+}
